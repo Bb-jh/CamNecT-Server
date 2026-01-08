f66827e900fed58384ba0df4a449cada1b99e784
seungmo <boorue03@gmail.com>
2026-01-08 15:51:52 +0900

    fix:Login관련기능 추가

--- src/main/java/CamNecT/CamNecT_Server/domain/auth/controller/AuthController.java
+++ src/main/java/CamNecT/CamNecT_Server/domain/auth/controller/AuthController.java
@@ -1,7 +1,10 @@
 package CamNecT.CamNecT_Server.domain.auth.controller;
 
+import CamNecT.CamNecT_Server.domain.auth.dto.login.LoginRequest;
+import CamNecT.CamNecT_Server.domain.auth.dto.login.LoginResponse;
 import CamNecT.CamNecT_Server.domain.auth.dto.signup.SignupRequest;
 import CamNecT.CamNecT_Server.domain.auth.dto.signup.SignupResponse;
+import CamNecT.CamNecT_Server.domain.auth.service.LoginService;
 import CamNecT.CamNecT_Server.domain.auth.service.SignupService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +17,19 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final SignupService signupService;
+    private final LoginService loginService;
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public SignupResponse signup(@RequestBody @Valid SignupRequest req) {
         return signupService.signup(req);
     }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody @Valid LoginRequest req) {
+        return loginService.login(req);
+    }
+
 
     @GetMapping("/email/verify")
     @ResponseStatus(HttpStatus.NO_CONTENT)
