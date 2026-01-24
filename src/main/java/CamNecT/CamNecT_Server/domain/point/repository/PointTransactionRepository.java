64d1b70b7bc54a56df56fb6e729e75b098a7aa05
seungmo <boorue03@gmail.com>
2026-01-24 14:14:13 +0900

    fix : 포인트 spend 수정 및 관련 클래스들 수정

--- src/main/java/CamNecT/CamNecT_Server/domain/point/repository/PointTransactionRepository.java
+++ src/main/java/CamNecT/CamNecT_Server/domain/point/repository/PointTransactionRepository.java
@@ -4,4 +4,5 @@ import CamNecT.CamNecT_Server.domain.point.model.PointTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PointTransactionRepository extends JpaRepository<PointTransaction, Long> {
+    boolean existsByEventKey(String eventKey);
 }
