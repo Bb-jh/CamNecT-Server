3ce2b3aca5cefa0fbd4d30f220d1f2cd3f44ccd3
seungmo <boorue03@gmail.com>
2026-01-25 09:55:44 +0900

    fix : User 하위 도메인(Home) Error처리 일반화(세분화)

--- src/main/java/CamNecT/CamNecT_Server/domain/home/service/HomeService.java
+++ src/main/java/CamNecT/CamNecT_Server/domain/home/service/HomeService.java
@@ -3,6 +3,8 @@ package CamNecT.CamNecT_Server.domain.home.service;
 import CamNecT.CamNecT_Server.domain.home.dto.HomeResponse;
 import CamNecT.CamNecT_Server.domain.users.model.Users;
 import CamNecT.CamNecT_Server.domain.users.repository.UserRepository;
+import CamNecT.CamNecT_Server.global.common.exception.CustomException;
+import CamNecT.CamNecT_Server.global.common.response.errorcode.bydomains.UserErrorCode;
 import CamNecT.CamNecT_Server.global.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +19,7 @@ public class HomeService {
         // TODO: deviceId로 user 조회해서 displayName 결정
         // TODO: notification/coffeeChat/schedule/point/alumni/contest 각 도메인 QueryService 연결
         Users user = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new IllegalArgumentException("user not found: " + userId));
+                .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
         long unreadCount = notificationService.countUnread(user.getUserId());
         return HomeResponse.of(user.getName(), unreadCount);
     }
