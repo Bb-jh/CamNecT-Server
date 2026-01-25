e5b3c5aa497c723f54f69c92253e241d277af1a4
seungmo <boorue03@gmail.com>
2026-01-25 10:33:28 +0900

    refractor: 코드 정리

--- src/main/java/CamNecT/CamNecT_Server/global/common/service/S3Service.java
+++ src/main/java/CamNecT/CamNecT_Server/global/common/service/S3Service.java
@@ -2,18 +2,8 @@ package CamNecT.CamNecT_Server.global.common.service;
 
 import CamNecT.CamNecT_Server.global.storage.FileStorage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-
-import java.io.IOException;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -32,7 +22,7 @@ public class S3Service {
         return fileStorage.save(prefix, file); // key 리턴
     }
 
-    public void deleteFileByKey(String key) {
+    public void deleteFile(String key) {
         fileStorage.delete(key);
     }
 }
